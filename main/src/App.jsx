cba57185481c549037e45d880c92abad9afe1c51
Adil-654 <adilahmadshah7860@gmail.com>
2024-01-25 16:55:59 +0530

    first commit

---
+++ main/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import kgButton from './kgButton'
+function App() {
+  return 
+    <div>
+     <h1>hello</h1>
+   <kgButton>  </kgButton>
+    </div>
+  
+}
+
+export default App
---
+++ main/src/kgButton.jsx
@@ -0,0 +1,4 @@
+function kgButton(){
+    return <h1>adil</h1>
+}
+export default kgButton
